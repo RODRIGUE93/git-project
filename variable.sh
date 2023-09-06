ffa12befd371d8ddcd83e0996afbb3fb3d656902
edmond <edmondton99@gmail.com>
2023-09-06 01:53:27 -0500

    new script

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+   #Author: Rodrigue Edmond
+   #Date: 09/05/2023
+
+   #Packages installation on CentOs server
+
+   echo "Hello there, your username is ${USER}"
+   echo "Your user ID is ${ID}"
+   echo "Your shell is ${SHELL}"
+   echo "Your home directory is ${HOME}
+   echo This server Hostname is ${HOSTNAME}  
